c77fc7609072f06f25f6f57acfd8c7d0905902ae
Ziz <zizsdl@googlemail.com>
2011-06-11 15:53:43 +0200

    Added .gitignore

--- puzzletube.c
+++ puzzletube.c
@@ -4,7 +4,7 @@ int main(int argc, char **argv)
 {
   srand(time(NULL));
   initEngine();
-  prepare_game_objects(1,4);
+  prepare_game_objects(1,6);
   run_game(1,timeMode,0);
   delete_game_objects();
   quitEngine();
