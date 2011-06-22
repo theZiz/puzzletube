bf80feafcca5b9c3bfc2c83ef7441e7e98e95f64
Ziz <zizsdl@googlemail.com>
2011-06-22 23:02:25 +0200

    - Added lines for the licence - Added some tweaks for the gp2x - renamed the music

--- puzzletube.c
+++ puzzletube.c
@@ -1,3 +1,22 @@
+/*
+ The contents of this file are subject to the Mozilla Public License        
+ Version 1.1 (the "License"); you may not use this file except in           
+ compliance with the License. You may obtain a copy of the License at       
+ http://www.mozilla.org/MPL/                                                
+                                                                            
+ Software distributed under the License is distributed on an "AS IS"        
+ basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the    
+ License for the specific language governing rights and limitations         
+ under the License.                                                         
+                                                                            
+ Alternatively, the contents of this file may be used under the terms       
+ of the GNU Lesser General Public license (the  "LGPL License"), in which case the  
+ provisions of LGPL License are applicable instead of those                  
+ above.                                                                     
+                                                                            
+ For feedback and questions about my Files and Projects please mail me,     
+ Alexander Matthes (Ziz) , zizsdl_at_googlemail.com                         
+*/
 #include <SDL/SDL_mixer.h>
 #include "game.h"
 
@@ -13,7 +32,7 @@ int main(int argc, char **argv)
   #else
     Mix_OpenAudio(44100,AUDIO_S16SYS,2,2048);
   #endif
-  music = Mix_LoadMUS("./sounds/Puzzle_game_tune_2.ogg");
+  music = Mix_LoadMUS("./sounds/Cosmic Conundrum.ogg");
   Mix_PlayMusic(music,-1);
   prepare_game_objects(1,6);
   run_game(1,timeMode,0);
