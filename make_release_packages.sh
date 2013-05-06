a77b539cf9f6ee51de6b44f26c686b1ab70dee7b
Ziz <zizsdl@googlemail.com>
2013-05-06 13:31:30 +0200

    Improved the script even more!

--- make_release_packages.sh
+++ make_release_packages.sh
@@ -1,44 +1,48 @@
 #!/bin/sh
 # Change the folder to YOUR sparrow3d folder!
+PROGRAM="puzzletube"
 VERSION="1.2.0.0"
 DEST=./build/*
 echo "<html>" > index.htm
 echo "<head>" >> index.htm
 echo "</head>" >> index.htm
 echo "<body>" >> index.htm
-echo "<h1>Puzzletube Download links:</h1>" >> index.htm
+echo "<h1>$PROGRAM download links:</h1>" >> index.htm
 for f in $DEST
 do
-  if [ -e "$f/puzzletube/puzzletube" ]; then
-  NAME=`echo "$f" | cut -d/ -f3 | cut -d. -f1`
-    echo "Copy to: $NAME"
-    cp -r font "$f/puzzletube"
-    cp -r images "$f/puzzletube"
-    cp -r music "$f/puzzletube"
-    cp -r sounds "$f/puzzletube"
-    cp -r translations "$f/puzzletube"
+  if [ -e "$f/$PROGRAM/$PROGRAM" ]; then
+    NAME=`echo "$f" | cut -d/ -f3 | cut -d. -f1`
+    echo "$NAME:"
+    echo "--> Copy temporary folders"
+    cp -r font "$f/$PROGRAM"
+    cp -r images "$f/$PROGRAM"
+    cp -r music "$f/$PROGRAM"
+    cp -r sounds "$f/$PROGRAM"
+    cp -r translations "$f/$PROGRAM"
     cd $f
+    echo "--> Create archive"
     if [ $NAME = "pandora" ]; then
-      cd puzzletube
+      cd $PROGRAM
       ../make_package.sh
       cd ..
-		  echo "<a href=puzzletube.pnd>$NAME</a></br>" >> ../../index.htm
+		  echo "<a href=$PROGRAM.pnd>$NAME</a></br>" >> ../../index.htm
     else
       if [ $NAME = "i386" ]; then
-        tar cfvz "puzzletube-$NAME-$VERSION.tar.gz" puzzletube
-        mv "puzzletube-$NAME-$VERSION.tar.gz" ../..
-  		  echo "<a href=puzzletube-$NAME-$VERSION.tar.gz>$NAME</a></br>" >> ../../index.htm
+        tar cfvz "$PROGRAM-$NAME-$VERSION.tar.gz" $PROGRAM > /dev/null
+        mv "$PROGRAM-$NAME-$VERSION.tar.gz" ../..
+  		  echo "<a href=$PROGRAM-$NAME-$VERSION.tar.gz>$NAME</a></br>" >> ../../index.htm
       else
-        zip -r "puzzletube-$NAME-$VERSION.zip" puzzletube
-        mv "puzzletube-$NAME-$VERSION.zip" ../..
-  		  echo "<a href=puzzletube-$NAME-$VERSION.zip>$NAME</a></br>" >> ../../index.htm
+        zip -r "$PROGRAM-$NAME-$VERSION.zip" $PROGRAM > /dev/null
+        mv "$PROGRAM-$NAME-$VERSION.zip" ../..
+  		  echo "<a href=$PROGRAM-$NAME-$VERSION.zip>$NAME</a></br>" >> ../../index.htm
       fi
     fi
-    rm -r puzzletube/font
-    rm -r puzzletube/images
-    rm -r puzzletube/music
-    rm -r puzzletube/sounds
-    rm -r puzzletube/translations
+    echo "--> Remove temporary folders"
+    rm -r $PROGRAM/font
+    rm -r $PROGRAM/images
+    rm -r $PROGRAM/music
+    rm -r $PROGRAM/sounds
+    rm -r $PROGRAM/translations
     cd ..
     cd ..
   fi
